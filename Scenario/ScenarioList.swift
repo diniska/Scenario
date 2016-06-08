1e3933fd339ee8392860c73c10fd1d067119b58d
Denis Chaschin <denis@chaschin.pro>
2016-06-08 21:05:36 +0300

    callbacks removed as unwanted

--- Scenario/ScenarioList.swift
+++ Scenario/ScenarioList.swift
@@ -6,15 +6,13 @@
 //  Copyright © 2016 diniska. All rights reserved.
 //
 
-import Foundation
-
+import UIKit
 
 public struct ScenarioList {
     public typealias ScenarioName = String
 
     private var items: [ScenarioName: Scenario] = [:]
     private var parameters: [ScenarioName: ScenarioParameters] = [:]
-    private var callbacks: [ScenarioName: ScenarioResulCallback] = [:]
 
     public subscript(name: ScenarioName) -> Scenario? {
         get {
@@ -25,12 +23,11 @@ public struct ScenarioList {
         }
     }
 
-    public func performScenario(with name: ScenarioName, from viewController: UIViewController) {
+    public func performScenario(with name: ScenarioName, from viewController: UIViewController, callback:ScenarioResultCallback) {
         guard let scenario = self[name]
             else { debugPrint("There is no scenario with name \(name) in scenario list"); return }
 
         let scenarioParameters = parameters[name]
-        let callback = callbacks[name]
 
         scenario.perfrom(from: viewController, with: scenarioParameters, callback: callback)
     }
@@ -38,13 +35,11 @@ public struct ScenarioList {
     public mutating func addPerformer(performer: ScenarioPerformer, with name: ScenarioName) {
         self[name] = performer.scenario
         self.parameters[name] = performer.parameters
-        self.callbacks[name] = performer.callback
     }
 
     public struct ScenarioPerformer {
         public let scenario: Scenario
         public var parameters: ScenarioParameters?
-        public var callback: ScenarioResulCallback?
 
         public init(scenario: Scenario) {
             self.scenario = scenario
